5e9837dbfc5ee2da63df769fa752c3c4414fb3ac
ninisha1 <86066747+ninisha1@users.noreply.github.com>
2021-06-20 12:05:09 +0530

    Create Circle extend.py

---
+++ Circle extend.py
@@ -0,0 +1,12 @@
+1.r=float(input ("Enter the value of radius ")
+Area=3.14*r*r
+Print("area of circle",area)
+
+
+
+2.import os
+split_tup = os.path.splitext('abc.py.python') 
+file_name = split_tup[0] 
+file_extension = split_tup[1] 
+print("File Name: ", file_name) 
+print("File Extension: ", file_extension)
